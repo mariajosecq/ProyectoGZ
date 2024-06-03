808194d7e1ed649d0a8522ce74d0e9c174554dcc
mariajosecq <cote21cq@gmail.com>
2024-06-03 11:48:50 -0400

    Se elimina index.html. Se agregan html del proyecto, modificando también urls.py y views.py

--- GrupoZero/urls.py
+++ GrupoZero/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index.html', views.index, name='index'),
-
+    path('principal.html', views.principal, name='principal'),
+    path('perfil_usuario.html', views.perfil_usuario, name='perfil_usuario'),
+    path('perfil_admin.html', views.perfil_admin, name='perfil_admin'),
+    path('detalle_obra.html', views.detalle_obra, name='detalle_obra'),
+    path('detalle_autor.html', views.detalle_autor, name='detalle_autor'),
+    path('detalle_autor2.html', views.detalle_autor2, name='detalle_autor2'),
+    path('detalle_autor3.html', views.detalle_autor3, name='detalle_autor3'),
+    path('blog.html', views.blog, name='blog'),
 ]
