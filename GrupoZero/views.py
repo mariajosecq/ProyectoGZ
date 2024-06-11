b0a2a24c15e6fb0627eb0805ae07e46d1819bf91
mariajosecq <cote21cq@gmail.com>
2024-06-11 01:00:20 -0400

    Se crean tablas en models.py, se hacen migraciones, se crea super usuario modificando admin.py. Se crea vista de registro_inicio_sesion en views.py

--- GrupoZero/views.py
+++ GrupoZero/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -37,3 +39,31 @@ def blog(request):
 def base(request):
     context={}
     return render(request, 'GrupoZero/base.html',context)
+
+def registro_inicio_sesion(request):
+    if request.method == 'POST':
+        # Verifica si se está realizando un registro o un inicio de sesión
+        if 'registro' in request.POST:
+            # Registro de nuevo usuario
+            email = request.POST.get('email_registro')
+            username = request.POST.get('nombre_registro')
+            password = request.POST.get('contrasena_registro')
+            user = User.objects.create_user(username=username, email=email, password=password)
+            # Redirige al usuario a la página de éxito después del registro
+            return redirect('pagina_exito')
+        elif 'inicio_sesion' in request.POST:
+            # Inicio de sesión de usuario existente
+            email = request.POST.get('email_inicio_sesion')
+            password = request.POST.get('contrasena_inicio_sesion')
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirige al usuario a la página de éxito después del inicio de sesión
+                return redirect('pagina_exito')
+            else:
+                # El inicio de sesión falló, puedes agregar un mensaje de error
+                # y renderizar nuevamente el formulario de inicio de sesión
+                return render(request, 'GrupoZero/base.html', {'error': 'Inicio de sesión fallido'})
+    
+    # Si el método no es POST, simplemente renderiza el formulario
+    return render(request, 'GrupoZero/base.html')
